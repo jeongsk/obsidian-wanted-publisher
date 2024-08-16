295d6d1f41f952bccd56181d7249d18b375293da
jeongsk <jeongseok@wantedlab.com>
2024-08-16 12:20:22 +0900

    feat(main.ts): 게시물 제목 및 콘텐츠 형식 가져오기 기능 추가 fix(main.ts): 비어 있는 노트 처리 및 Frontmatter 처리 로직 개선

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
-import { App, MarkdownView, Modal, Notice, Plugin } from "obsidian";
+import {
+	FrontMatterCache,
+	MarkdownView,
+	Notice,
+	Plugin,
+	TFile,
+	stringifyYaml,
+} from "obsidian";
+import Client from "./client";
 import { DEFAULT_SETTINGS, WantedPublisherPluginSettings } from "./constants";
+import { getContentWithoutFrontmatter } from "./helpers/frontmatter";
 import WantedPublisherSettingTab from "./setting-tab";
 
 export default class WantedPublisherPlugin extends Plugin {
@@ -14,27 +23,64 @@ export default class WantedPublisherPlugin extends Plugin {
 			callback: async () => {
 				const markdownView =
 					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (!markdownView?.file) {
+				const activeFile = markdownView?.file;
+				if (!activeFile) {
 					return new Notice(
 						"No open note found. Please open a note to publish.",
 					);
 				}
-				const body = await this.app.vault.read(markdownView?.file);
-				if (!body?.trim()) {
-					return new Notice("Empty note. Please write something to publish.");
+				const title = activeFile.basename;
+				const formattedContent = getContentWithoutFrontmatter(
+					await this.app.vault.read(activeFile),
+				);
+				if (!formattedContent) {
+					return new Notice(
+						"Empty note. Please write something to publish.",
+					);
 				}
 
-				console.log(body);
+				const frontmatter =
+					this.app.metadataCache.getFileCache(activeFile)
+						?.frontmatter || {};
+				const postId = frontmatter.socialPostId;
+
+				const client = new Client(this.settings.token);
+
+				if (postId) {
+					await client.updatePost({
+						postId,
+						title,
+						coverImageKey: "",
+						formattedContent,
+						bodyImageKeys: [],
+					});
+				} else {
+					const results = await client.publishPost({
+						title,
+						coverImageKey: "",
+						formattedContent,
+						bodyImageKeys: [],
+					});
+					frontmatter["socialPostId"] = results.postId;
+					const newFileContent = `---\n${stringifyYaml(frontmatter)}\n---\n${formattedContent}`;
+					await this.app.vault.modify(activeFile, newFileContent);
+				}
 			},
 		});
 
 		this.addSettingTab(new WantedPublisherSettingTab(this.app, this));
 	}
 
+	async processFrontMatter(file: TFile): Promise<FrontMatterCache> {
+		return new Promise((resolve) =>
+			this.app.fileManager.processFrontMatter(file, resolve),
+		);
+	}
+
 	async loadSettings() {
 		this.settings = {
 			...DEFAULT_SETTINGS,
-			...await this.loadData()
+			...(await this.loadData()),
 		};
 	}
 

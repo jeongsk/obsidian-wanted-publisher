3fc9c08ef1a0b98ea0f012c536247fbde4765e52
jeongsk <jeongseok@wantedlab.com>
2024-08-16 11:45:01 +0900

    refactor(client.ts): 인터페이스 이름 변경 및 게시물 발행 메서드 수정 feat(client.ts): API 요청을 위한 makeRequest 메서드 추가 및 응답 처리 개선

--- src/client.ts
+++ src/client.ts
@@ -1,27 +1,38 @@
-import { requestUrl } from "obsidian";
+import { requestUrl, RequestUrlResponse } from "obsidian";
 
-const API_URL = process.env.API_URL ?? "http://localhost:3000";
-const COOKIE_NAME = process.env.COOKIE_NAME ?? "accessToken";
+const API_URL = process.env.API_URL || "http://localhost:3000";
+const COOKIE_NAME = process.env.COOKIE_NAME || "accessToken";
 
-interface Post {
+interface PostContent {
 	title: string;
 	coverImageKey: string;
 	formattedContent: string;
 	bodyImageKeys: string[];
 }
 
-export default class Client {
-	constructor(private readonly accessToken: string) { }
+interface PublishResponse {
+    postId: number;
+}
 
-	async publisher(post: Post) {
-		return await requestUrl({
-			url: API_URL,
+export default class Client {
+    constructor(private readonly accessToken: string) {}
+
+    async publishPost(post: PostContent): Promise<PublishResponse> {
+        const response = await this.makeRequest<PublishResponse>("/publish", post);
+        return response;
+    }
+
+    private async makeRequest<T, P = any>(endpoint: string, data: P): Promise<T> {
+        const response: RequestUrlResponse = await requestUrl({
+            url: `${API_URL}${endpoint}`,
 			method: "POST",
 			contentType: "application/json",
-			body: JSON.stringify(post),
+            body: JSON.stringify(data),
 			headers: {
 				"Cookie": `${COOKIE_NAME}=${this.accessToken}`
 			}
-		}).then((res) => res.json() as { postId: number });
+        });
+
+        return response.json as T;
 	}
 }
